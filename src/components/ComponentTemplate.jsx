523d46e0777363d216d722faa434d17dce8e3098
paku <p4blo.kuhn@gmail.com>
2024-06-21 18:14:08 -0300

    fixed codeblock overflow in mobile

--- src/components/ComponentTemplate.jsx
+++ src/components/ComponentTemplate.jsx
@@ -1,8 +1,8 @@
 export default function ComponentTemplate({ children, title, text }) {
   return (
     <>
-      <section className="relative flex h-full items-start justify-center px-2 xl:w-full">
-        <div className="flex flex-col gap-5 xl:w-[36em]">
+      <section className="relative flex h-full w-full items-start justify-center px-2">
+        <div className="flex w-full flex-col gap-5 xl:w-[36em]">
           <span className="h-1 w-full bg-gradient-to-r from-background via-gray-700 to-background"></span>
           <div className="flex w-full w-max">
             <h2 className="title">{title}</h2>
