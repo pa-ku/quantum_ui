df897f74d4cd120a249e0435148990eaf776c79e
paku <p4blo.kuhn@gmail.com>
2024-06-22 17:29:37 -0300

    updated chroma btn

--- src/components/buttons/ChromaBtn.jsx
+++ src/components/buttons/ChromaBtn.jsx
@@ -9,7 +9,7 @@ export default function ChromaBtn() {
 
   return (
     <>
-      <button className="animate-chroma rounded-md bg-[#327861] p-2 text-xl text-black duration-100">
+      <button className="bg-primary-300 text-background font-bold chroma rounded-md px-4 py-3 text-xl duration-100">
         Chroma Button
       </button>
       <CodeBlock code={codeBlock} />
