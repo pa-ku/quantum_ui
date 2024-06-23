c8c06b62d4e88d8329dce1d998338e23475bb418
paku <p4blo.kuhn@gmail.com>
2024-06-23 14:58:48 -0300

    added new section to bentobox

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,52 +9,6 @@ export default function Home() {
   );
 }
 
-function BentoBox() {
-  return (
-    <>
-      <div className="grid">
-        <div className="box">
-          <div className="content">
-            <svg
-              className="h-full"
-              viewBox="0 0 24 24"
-              strokeWidth="0.5"
-              stroke="currentColor"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M11.667 6c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 2 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968zm-4 6.5c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 1.975 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968z" />
-            </svg>
-          </div>
-        </div>
-
-        <div className="box">
-          <div className="content p-3">
-            <svg
-            className="h-full w-full"
-              viewBox="0 0 24 24"
-              stroke-width="0.5"
-              stroke="#ffffff"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M13 3l0 7l6 0l-8 11l0 -7l-6 0l8 -11" />
-            </svg>
-          </div>
-        </div>
-        <div className="box span">
-          <div className="content text-3xl">Pure Css Components</div>
-        </div>
-        <span className="chroma"></span>
-      </div>
-    </>
-  );
-}
-
 function Banner() {
   return (
     <>
@@ -70,43 +24,70 @@ function Banner() {
   );
 }
 
-function Cards() {
+function BentoBox() {
   return (
     <>
-      <div className="grid grid-cols-2 gap-2 p-3 text-secundary-200 md:p-0">
-        <div className="rounded-xl bg-[#1d222f] p-10">
-          <svg
-            className="h-full"
-            viewBox="0 0 24 24"
-            strokeWidth="0.5"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M11.667 6c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 2 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968zm-4 6.5c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 1.975 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968z" />
-          </svg>
+      <div className="grid">
+        <div className="box">
+          <div className="content relative h-full min-w-full">
+            <svg
+              className="p-5"
+              viewBox="0 0 24 24"
+              strokeWidth="0.5"
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M11.667 6c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 2 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968zm-4 6.5c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 1.975 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968z" />
+            </svg>
+            <p className="absolute bottom-5">Tailwind</p>
+          </div>
+        </div>
+        <div className="box">
+          <div className="content relative h-full w-full">
+            <svg
+              className="p-10"
+              viewBox="0 0 24 24"
+              stroke-width="1"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+              <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
+              <circle cx="12" cy="7.5" r=".5" fill="currentColor" />
+            </svg>
+            <p className="absolute bottom-5">Accesible</p>
+          </div>
         </div>
 
-        <div className="rounded-xl bg-[#1d222f] p-10">
-          <svg
-            className="h-full"
-            viewBox="0 0 24 24"
-            strokeWidth="0.5"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z" />
-            <path d="M8.5 8h7l-4.5 4h4l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5" />
-          </svg>
+        <div className="box row-span-2 h-full">
+          <div className="content h-full w-full">
+            <svg
+              className="h-full w-full"
+              viewBox="0 0 24 24"
+              stroke-width="0.5"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M13 3l0 7l6 0l-8 11l0 -7l-6 0l8 -11" />
+            </svg>
+            <p className="absolute bottom-5 text-xl">Fast animations </p>
+          </div>
         </div>
-        <div className="col-span-2 rounded-xl bg-[#1d222f] p-10 text-center">
-          <h3 className="text-3xl">Pure Css Components</h3>
+
+        <div className="box span">
+          <div className="content h-20 text-3xl">Pure Css Components</div>
         </div>
+
+        <span className="chroma"></span>
       </div>
     </>
   );
