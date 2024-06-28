222da93bcb72a4b792fbf0829b50f76a7e905170
paku <p4blo.kuhn@gmail.com>
2024-06-28 14:21:17 -0300

    update minimal input border

--- src/components/inputs/Minimal_Input.jsx
+++ src/components/inputs/Minimal_Input.jsx
@@ -3,7 +3,7 @@ import CodeBlock from "../CodeBlock";
 export default function MinimalInput() {
   const codeBlock = `<input
     type="text"
-    className="rounded-xl border-[1px] border-transparent border-white 
+    className="rounded-xl border-[1px] border-transparent 
       bg-gray-800 px-4 py-2 
       text-white outline-none 
       placeholder:text-gray-200 
@@ -15,7 +15,7 @@ export default function MinimalInput() {
     <>
       <input
         type="text"
-        className="rounded-xl border-[1px] border-transparent border-white bg-gray-800 px-4 py-2 text-white outline-none placeholder:text-gray-200 focus:bg-gray-800 focus-visible:border-[#24c295]"
+        className="rounded-xl border-[1px] border-transparent bg-gray-800 px-4 py-2 text-white outline-none placeholder:text-gray-200 focus:bg-gray-800 focus-visible:border-[#24c295]"
         placeholder="Input"
       />
       <CodeBlock code={codeBlock} />
