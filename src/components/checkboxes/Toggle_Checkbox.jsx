2ce8b85c831911ac6aeb16d1da3d889dc4bbec5c
paku <p4blo.kuhn@gmail.com>
2024-07-23 01:53:40 -0300

    fixed toggle button typo

--- src/components/checkboxes/Toggle_Checkbox.jsx
+++ src/components/checkboxes/Toggle_Checkbox.jsx
@@ -4,7 +4,7 @@ import CodeBlock from "../CodeBlock";
 export default function ToggleBtn() {
   const codeBlock = `<input
   type="checkbox"
-  className="before:contets-['asd'] relative flex h-6 w-12
+  className="relative flex h-6 w-12
   cursor-pointer appearance-none items-center rounded-xl
   bg-white duration-200 before:pointer-events-none 
   before:absolute before:h-4 before:w-4 before:translate-x-1
@@ -16,7 +16,7 @@ export default function ToggleBtn() {
     <>
       <input
         type="checkbox"
-        className="before:contets-['asd'] relative flex h-6 w-12 cursor-pointer appearance-none items-center rounded-xl bg-white duration-200 before:pointer-events-none before:absolute before:h-4 before:w-4 before:translate-x-1 before:rounded-xl before:bg-primary-500 before:duration-200 checked:bg-primary-300 checked:shadow-center checked:shadow-primary-700 checked:before:translate-x-7 checked:before:bg-white"
+        className="relative flex h-6 w-12 cursor-pointer appearance-none items-center rounded-xl bg-white duration-200 before:pointer-events-none before:absolute before:h-4 before:w-4 before:translate-x-1 before:rounded-xl before:bg-primary-500 before:duration-200 checked:bg-primary-300 checked:shadow-center checked:shadow-primary-700 checked:before:translate-x-7 checked:before:bg-white"
       />
       <CodeBlock code={codeBlock} />
     </>
