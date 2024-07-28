7b7f618d0c2155f7db032dae696850a42fe49134
paku <p4blo.kuhn@gmail.com>
2024-07-27 23:26:19 -0300

    action button improved size, and removed danger action button

--- src/components/buttons/Action_button.jsx
+++ src/components/buttons/Action_button.jsx
@@ -14,10 +14,7 @@ export default function ActionBtn() {
   return (
     <>
       <div className="space-x-5">
-        <button className="hover:[card-glowing-hover] rounded-md bg-primary-500 p-2 text-xl text-white shadow-bottom shadow-primary-800 duration-100 active:translate-y-1 active:shadow-none">
-          Push
-        </button>
-        <button className="hover:[card-glowing-hover] h-20 w-20 rounded-full bg-red-700 p-2 text-xl text-white shadow-bottom shadow-red-900 duration-100 active:translate-y-1 active:shadow-none">
+        <button className="hover:[card-glowing-hover] rounded-md bg-primary-500 px-5 py-3 text-xl text-white shadow-bottom shadow-primary-800 duration-100 active:translate-y-1 active:shadow-none">
           Push
         </button>
       </div>
