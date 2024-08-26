712e1062f1bd485394c8982dd238287cec3ecd93
paku <p4blo.kuhn@gmail.com>
2024-08-26 16:43:08 -0300

    removed input passowrd from modern

--- src/components/inputs/Modern_input.tsx
+++ src/components/inputs/Modern_input.tsx
@@ -31,18 +31,6 @@ export default function Modern_input() {
           Email
         </p>
       </div>
-
-      <div className="relative flex h-max w-max items-center justify-start">
-        <input
-          type="password"
-          className="text-red caret-primary focus-visible:border-primary peer rounded-xl border-[1px] border-transparent bg-gray-800 px-3 py-2 outline-none"
-          placeholder=" "
-        />
-        <p className="pointer-events-none absolute -translate-y-8 px-3 text-slate-300 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:text-slate-400 peer-focus:-translate-y-8 peer-focus:text-slate-300">
-          Password
-        </p>
-      </div>
-
       <CodeBlock code={codeBlock} />
     </>
   );
