e625bc026ac243f6ecc6cfaa0dd3d1c42c42a831
paku <p4blo.kuhn@gmail.com>
2024-08-26 03:49:37 -0300

    animation in change colors

--- src/components/ui/ColorHandler.tsx
+++ src/components/ui/ColorHandler.tsx
@@ -11,7 +11,7 @@ export default function ColorHandler({
 }: ColorTypes) {
   return (
     <label
-      className={`${children === "Primary" ? "bg-primary" : "bg-secundary"} flex w-40 cursor-pointer flex-col items-center justify-center p-4`}
+      className={`${children === "Primary" ? "bg-primary" : "bg-secundary"} flex w-40 cursor-pointer flex-col items-center justify-center p-4 duration-200 hover:-translate-y-1`}
     >
       <p className="font-bold"> {children}</p>
       <input
