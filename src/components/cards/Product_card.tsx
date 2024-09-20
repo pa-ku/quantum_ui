bd24b6bceeaa92ad75dc03733bacdf0a682a6fed
paku <p4blo.kuhn@gmail.com>
2024-09-19 23:14:39 -0300

    updated product card

--- src/components/cards/Product_card.tsx
+++ src/components/cards/Product_card.tsx
@@ -5,7 +5,7 @@ import jsxToString from 'react-element-to-jsx-string'
 export default function Product_card() {
   const componentToJsx = (
     <figure className="relative w-56 shadow-md shadow-gray-900">
-      <div className="rounded-t-xl bg-white">
+      <div className="rounded-t-xl border-b-2 bg-white">
         <span
           aria-label="Descuento"
           className="absolute right-0 rounded-bl-xl rounded-tr-xl bg-primary px-3 text-xl font-bold"
@@ -18,10 +18,13 @@ export default function Product_card() {
           alt="Product image"
         />
       </div>
-      <figcaption className="rounded-b-xl bg-white p-2">
-        <p className="text-start text-xl font-bold text-black">$1.000</p>
-        <p className="font-bold text-gray-400 line-through">$150</p>
-        <h2 className="font-bold text-slate-700">Segway Ninebot Gt2</h2>
+      <figcaption className="rounded-b-xl bg-white p-2 text-slate-700">
+        <div className="flex items-center gap-1">
+          <p className="text-2xl font-bold">$1.000</p>
+          <p className="text-gray-500 line-through">$150</p>
+        </div>
+        <h2 className="text-lg font-bold">Segway Ninebot Gt2</h2>
+        <p className="">Lo mejor de su clase con una autonomia de 100km</p>
       </figcaption>
     </figure>
   )
