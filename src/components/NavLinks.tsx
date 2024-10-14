c4e9ba93ea521964a09a79a6eca07db778e3c275
Pαblo Kuhn <p4blo.kuhn@gmail.com>
2024-10-14 10:00:07 -0300

    changed links layout

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -3,13 +3,13 @@ import { LinkButton } from './ui/LinkButton'
 export default function NavLinks() {
   return (
     <>
-      <LinkButton to={'/cards'}>Cards</LinkButton>
       <LinkButton to={'/buttons'}>Buttons</LinkButton>
-      <LinkButton to={'/checkboxes'}>Checkboxes</LinkButton>
       <LinkButton to={'/inputs'}>Inputs</LinkButton>
-      <LinkButton to={'/modals'}>Modals</LinkButton>
-      <LinkButton to={'/animations'}>Animations</LinkButton>
       <LinkButton to={"/forms"}>Forms</LinkButton> 
+      <LinkButton to={'/checkboxes'}>Checkboxes</LinkButton>
+      <LinkButton to={'/modals'}>Modals</LinkButton>
+      <LinkButton to={'/cards'}>Cards</LinkButton>
+      <LinkButton to={'/animations'}>Animations</LinkButton>
     </>
   )
 }
