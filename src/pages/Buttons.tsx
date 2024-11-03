d2436ae57cfdfddea5d036a710881959db53587d
Pαblo Kuhn <p4blo.kuhn@gmail.com>
2024-11-03 00:30:51 -0300

    Better component structure, easier to add new components, added new menu checkbox

--- src/pages/Buttons.tsx
+++ src/pages/Buttons.tsx
@@ -1,58 +1,107 @@
 import React from 'react'
 import ComponentLayout from '../components/ComponentLayout'
 import ComponentTitle from '../components/ComponentTitle'
-import {
-  BUTTON_ACTION,
-  BUTTON_FILL,
-  BUTTON_GLOW,
-  BUTTON_GLOW_INSIDE,
-  BUTTON_MINIMAL,
-  BUTTON_NEON,
-  BUTTON_SLICK,
-  BUTTON_SLICK_CSS,
-} from '../components/design/buttons'
 
 export default function Buttons() {
+  const buttonsArr = [
+    { title: 'Fill', component: BUTTON_FILL, description: '' },
+    { title: 'Minimal', component: BUTTON_MINIMAL, description: '' },
+    { title: 'Glow', component: BUTTON_GLOW, description: '' },
+    { title: 'Glow Inside', component: BUTTON_GLOW_INSIDE, description: '' },
+    {
+      title: 'Slick',
+      component: BUTTON_SLICK,
+      css: BUTTON_SLICK_CSS,
+      description:
+        'This button needs the @property from css to animate the colors of the background image. Changing the transition time creates different animations',
+    },
+    { title: 'Neon', component: BUTTON_NEON, description: '' },
+    { title: 'Action', component: BUTTON_ACTION, description: '' },
+  ]
+
   return (
     <>
       <ComponentTitle>Buttons</ComponentTitle>
-      <ComponentLayout
-        component={BUTTON_FILL}
-        title="Fill"
-        description=""
-      ></ComponentLayout>
-      <ComponentLayout
-        component={BUTTON_MINIMAL}
-        title="Minimal"
-        description=""
-      ></ComponentLayout>
-      <ComponentLayout
-        component={BUTTON_GLOW}
-        title="Glow"
-        description=""
-      ></ComponentLayout>
-      <ComponentLayout
-        component={BUTTON_GLOW_INSIDE}
-        title="Glow Inside"
-        description=""
-      ></ComponentLayout>
-      <ComponentLayout
-        component={BUTTON_SLICK}
-        title="Slick"
-        description="This button needs the @property from css to animate the colors of the background image. Changing the transition time creates different animations"
-        css={BUTTON_SLICK_CSS}
-      ></ComponentLayout>
-      <ComponentLayout
-        component={BUTTON_NEON}
-        title="Neon"
-        description=""
-      ></ComponentLayout>
-
-      <ComponentLayout
-        component={BUTTON_ACTION}
-        title="Action"
-        description=""
-      ></ComponentLayout>
+      {buttonsArr.map(({ title, component, description, css }) => (
+        <ComponentLayout
+          component={component}
+          title={title}
+          description={description}
+          css={css && css}
+        ></ComponentLayout>
+      ))}
     </>
   )
 }
+
+const BUTTON_FILL = (
+  <button className="group relative overflow-hidden px-8 py-4 font-bold duration-500 hover:text-gray-700">
+    Documentation
+    <span className="absolute right-full top-0 -z-10 h-full w-full bg-white duration-500 ease-out group-hover:right-0"></span>
+  </button>
+)
+const BUTTON_MINIMAL = (
+  <button className="border-2 px-8 py-4 font-bold hover:bg-white hover:text-gray-700">
+    Learn more
+  </button>
+)
+const BUTTON_NEON = (
+  <button className="group relative flex h-max w-max items-center justify-center overflow-hidden rounded-xl p-0.5 mix-blend-lighten">
+    <p className="flex h-full w-full items-center justify-center rounded-lg bg-slate-800 px-8 py-4 text-xl text-emerald-500 duration-500">
+      Register
+    </p>
+    <span className="animate-rotate absolute -z-10 h-52 w-11 animate-spin rounded-full bg-gradient-to-r duration-500 group-hover:from-emerald-500"></span>
+  </button>
+)
+const BUTTON_SLICK = (
+  <button className="slick-button group relative rounded-md px-8 py-4 text-xl text-white duration-300">
+    Subscribe
+  </button>
+)
+const BUTTON_SLICK_CSS = `@property --color-1 {
+  syntax: "<color>";
+  inherits: true;
+  initial-value: #fff;
+}
+
+@property --color-2 {
+  syntax: "<color>";
+  inherits: true;
+  initial-value: #fff;
+}
+
+.slick-button {
+  --color-1: var(--primary);
+  --color-2: var(--secundary);
+  background-image:linear-gradient(40deg,var(--primary),var(--secundary));
+  transition:
+  --color-1 2000ms,
+  --color-2 800ms;
+}
+
+.slick-button:hover {
+  --color-1: var(--secundary);
+  --color-2: var(--primary);
+}
+`
+const BUTTON_GLOW = (
+  <button className="group relative flex h-max w-max items-center justify-center overflow-hidden rounded-lg bg-slate-800 p-0.5 mix-blend-lighten">
+    <p className="rounded-lg bg-gray-800 px-8 py-4 text-xl text-emerald-500 duration-300 group-hover:text-emerald-300">
+      Log In
+    </p>
+    <span className="animate-rotate absolute -z-10 size-48 animate-spin rounded-full bg-gradient-to-r duration-300 group-hover:from-emerald-500 group-hover:to-violet-500"></span>
+  </button>
+)
+const BUTTON_GLOW_INSIDE = (
+  <button className="group relative flex h-max w-max items-center justify-center overflow-hidden rounded-lg bg-gray-800 p-0.5 mix-blend-lighten">
+    <p className="rounded-lg px-8 py-4 text-xl text-emerald-500 duration-300 group-hover:text-emerald-300">
+      Join Us
+    </p>
+    <span className="animate-rotate absolute -z-10 size-48 animate-spin rounded-full bg-gradient-to-r duration-300 group-hover:from-emerald-500/10 group-hover:to-violet-500/20"></span>
+  </button>
+)
+const BUTTON_ACTION = (
+  <button className="rounded-md bg-gray-800 px-8 py-4 text-xl text-emerald-500 duration-100 [box-shadow:0px_0.25em_#000000] active:translate-y-1 active:shadow-none">
+    START GAME
+  </button>
+)
--- src/pages/CheckBoxes.tsx
+++ src/pages/CheckBoxes.tsx
@@ -1,66 +1,161 @@
 import React from 'react'
 import ComponentTitle from '../components/ComponentTitle'
-import {
-  CHECK_ACTIVE,
-  CHECK_DASHED,
-  CHECK_FILL,
-  CHECK_HEARTH,
-  CHECK_MINIMAL,
-  CHECK_PLUS,
-  CHECK_THEME,
-  CHECK_TOGGLE,
-} from '../components/design/checkbox'
+
 import ComponentLayout from '../components/ComponentLayout'
 export default function CheckBoxes() {
+  const checkArr = [
+    { title: 'Fill', component: CHECK_FILL, description: '' },
+    { title: 'Toggle', component: CHECK_TOGGLE, description: '' },
+    { title: 'Dashed', component: CHECK_DASHED, description: '' },
+    { title: 'Minimal', component: CHECK_MINIMAL, description: '' },
+    { title: 'Active', component: CHECK_ACTIVE, description: '' },
+    { title: 'Menu', component: CHECK_MENU, description: '' },
+    { title: 'Hearth', component: CHECK_HEARTH, description: '' },
+    { title: 'Plus', component: CHECK_PLUS, description: '' },
+    { title: 'Theme', component: CHECK_THEME, description: '' },
+  ]
+
   return (
     <>
       <ComponentTitle
         description={
-          ' All the inputs uses the Type Checkbox,hidding the default style with ::before. Mantaining all the benefits from the vanilla input'
+          ' All the inputs uses the Type Checkbox, mantaining the benefits of the vanilla input'
         }
       >
         CheckBoxes
       </ComponentTitle>
-      <ComponentLayout
-        component={CHECK_FILL}
-        title="Fill"
-        description=""
-      ></ComponentLayout>
-      <ComponentLayout
-        component={CHECK_TOGGLE}
-        title="Toggle"
-        description=""
-      ></ComponentLayout>
-      <ComponentLayout
-        component={CHECK_DASHED}
-        title="Dashed"
-        description=""
-      ></ComponentLayout>
-      <ComponentLayout
-        component={CHECK_MINIMAL}
-        title="Minimal"
-        description=""
-      ></ComponentLayout>
-      <ComponentLayout
-        component={CHECK_ACTIVE}
-        title="Active"
-        description=""
-      ></ComponentLayout>
-      <ComponentLayout
-        component={CHECK_HEARTH}
-        title="Favorite"
-        description="Minimalistic Add to favourites Button."
-      ></ComponentLayout>
-      <ComponentLayout
-        component={CHECK_PLUS}
-        title="Plus"
-        description="Used for clossing tags, or expansive containers."
-      ></ComponentLayout>
-      <ComponentLayout
-        component={CHECK_THEME}
-        title="Theme"
-        description=""
-      ></ComponentLayout>
+      {checkArr.map(({ title, component, description }) => (
+        <ComponentLayout
+          component={component}
+          title={title}
+          description={description}
+        ></ComponentLayout>
+      ))}
     </>
   )
 }
+
+const CHECK_MENU = (
+  <label className="relative flex items-center gap-1 px-1">
+    <input
+      type="checkbox"
+      className="group peer absolute h-full w-full cursor-pointer appearance-none"
+    />
+    <p className="">Open Menu</p>
+    <svg
+      className="size-7 duration-300 peer-checked:-rotate-180"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1"
+    >
+      <path d="M6 9l6 6l6 -6"></path>
+    </svg>
+  </label>
+)
+
+const CHECK_FILL = (
+  <label className="relative cursor-pointer overflow-hidden rounded-full [&_*]:rounded-full">
+    <input
+      className="peer absolute appearance-none font-bold"
+      type="checkbox"
+    />
+    <p className="border-2 px-5 py-2 duration-500 peer-checked:text-black">
+      Stay Calm
+    </p>
+    <span className="absolute right-full top-0 -z-10 h-full w-full bg-white duration-500 ease-out peer-checked:right-0"></span>
+  </label>
+)
+
+const CHECK_DASHED = (
+  <div className="relative">
+    <input
+      type="checkbox"
+      className="peer absolute h-full w-full cursor-pointer appearance-none"
+    />
+    <p className="rounded-lg border-2 border-dashed px-4 py-2 font-bold peer-checked:bg-white peer-checked:text-black">
+      Sound
+    </p>
+  </div>
+)
+const CHECK_MINIMAL = (
+  <div className="relative">
+    <input
+      type="checkbox"
+      className="peer absolute h-full w-full cursor-pointer appearance-none"
+    />
+    <p className="border-2 px-4 py-2 peer-checked:bg-white peer-checked:text-black">
+      Switch Off
+    </p>
+  </div>
+)
+
+const CHECK_ACTIVE = (
+  <div className="relative">
+    <input
+      type="checkbox"
+      className="peer absolute h-full w-full cursor-pointer appearance-none"
+    />
+    <p className="rounded-lg bg-gray-600 px-4 py-2 peer-checked:bg-emerald-500">
+      Activate
+    </p>
+  </div>
+)
+
+const CHECK_HEARTH = (
+  <label
+    htmlFor="favorite"
+    className="relative size-10 cursor-pointer stroke-emerald-500"
+  >
+    <input
+      id="favorite"
+      className="peer absolute appearance-none"
+      type="checkbox"
+    />
+    <svg
+      className="text-transparent duration-200 peer-checked:text-emerald-300"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      fill="currentColor"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+    </svg>
+  </label>
+)
+
+const CHECK_PLUS = (
+  <input
+    content="✖"
+    type="checkbox"
+    className="relative flex h-9 w-9 appearance-none items-center justify-center text-4xl before:absolute before:h-max before:w-max before:-rotate-45 before:cursor-pointer before:px-4 before:py-2 before:duration-200 before:content-[attr(content)] checked:before:rotate-0 checked:before:text-emerald-500"
+  />
+)
+
+const CHECK_THEME = (
+  <div className="relative flex">
+    <input
+      type="checkbox"
+      className="peer h-10 w-10 cursor-pointer appearance-none"
+    />
+    <img
+      className="pointer-events-none absolute invert duration-300 peer-checked:rotate-180 peer-checked:opacity-0"
+      src="https://cdn.icon-icons.com/icons2/1325/PNG/512/fun4x_86984.png"
+      alt="Sun Icon"
+    />
+    <img
+      className="pointer-events-none absolute -rotate-180 opacity-0 invert duration-300 peer-checked:rotate-0 peer-checked:opacity-100"
+      src="https://cdn.icon-icons.com/icons2/3255/PNG/512/moon_stars_weather_night_icon_205756.png"
+      alt="Moon Icon"
+    />
+  </div>
+)
+
+const CHECK_TOGGLE = (
+  <input
+    type="checkbox"
+    className="relative flex h-6 w-12 cursor-pointer appearance-none items-center rounded-xl bg-white duration-200 before:pointer-events-none before:absolute before:h-4 before:w-4 before:translate-x-1 before:rounded-xl before:bg-emerald-500 before:duration-200 checked:bg-emerald-500 checked:before:translate-x-7 checked:before:bg-white"
+  />
+)
