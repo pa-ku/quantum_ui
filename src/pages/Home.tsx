ebd5596b9c7c68d1d57749bc7ec59ef68ba7fa17
Pαblo Kuhn <p4blo.kuhn@gmail.com>
2024-12-15 02:00:11 -0300

    added maxw to bentobox

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <main className="relative flex w-full flex-col items-center justify-center gap-24 overflow-hidden px-3 py-10 md:w-[45em] lg:overflow-visible">
         <Banner />
-        <div className="w-full md:w-[40em]">
+        <div className="w-full md:max-w-[40em]">
           <BentoBox />
         </div>
         <Documentation />
